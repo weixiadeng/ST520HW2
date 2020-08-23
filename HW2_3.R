41a8a1d8763ad8776a575768f69d77932558c794
Weixia Deng <wdeng@ncsu.edu>
2020-08-23 08:47:41 -0400

    Upload R code

--- HW2_3.R
+++ HW2_3.R
@@ -8,7 +8,11 @@ inter <- df$logC[1]
 slr <- lm(logC ~ 0 + time, data = df, offset = rep(inter, length(df$logC)))
 beta0 <- round(df$logC[1], 3)
 beta1 <- round(-coef(slr), 3)
+beta1var <- round(summary(slr)$coefficients[2] ** 2, 6)
+res <- round(summary(slr)$sigma ** 2, 4)
 fit <- paste("log C(t) = ", beta0, " - ", beta1, " * t", sep = "")
+slopvari <- paste("Variance of slope:", beta1var)
+resvari <- paste("Residual variance:", res)
 
 png(filename = "HW2Q3.png", width = 500, height = 500, units = "px")
 plot(df$time, df$logC, pch = 19,
@@ -17,6 +21,5 @@ plot(df$time, df$logC, pch = 19,
      ylab = "Log - Drug concentration")
      
 abline(inter, coef(slr), col = "red")
-legend(0.5, -1.6, legend = c(fit), box.lty = 0, text.font = 3,
-       pt.cex = 10, cex = 1.5)
+legend(2, -1.6, legend = c(fit, slopvari, resvari), box.lty = 0, text.font = 3)
 dev.off()
